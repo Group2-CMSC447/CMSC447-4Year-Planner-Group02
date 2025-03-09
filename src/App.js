21f67ae1546012e239538da02616b12f22bca12c
Itsjustrick12 <Rickmjenkins900@gmail.com>
2025-03-08 20:41:33 -0500

    Created Credit Range Slider with App.js feedback on changes

--- src/App.js
+++ src/App.js
@@ -5,38 +5,46 @@ import CreditRange from './components/CreditRange'
 
 function App() {
     const [years, setYears] = useState([
-        { name: "Year 1" },
-        { name: "Year 2" },
-        { name: "Year 3" },
-        { name: "Year 4" }
+        { name: "Year 1"},
+        { name: "Year 2"},
+        { name: "Year 3"},
+        { name: "Year 4"}
     ]);
 
     const addYear = () => {
         const newYear = years.length + 1;
-        setYears([...years, { name: 'Year ' + newYear }]);
+        setYears([...years, { name: 'Year ' + newYear}]);
  
+    };
+    //Use for the credit range checks
+    const [min, setMin] = useState();
+    const [max, setMax] = useState();
+
+    const changeVals = (value) => {
+        console.log("Changed Vals to: " + value[0] + " and " + value[1])
+        setMin(value[0]);
+        setMax(value[1]);
     };
 
     return (
-      <div className="flex flex flex-col" data-testid="callYear">
-            <div>
-                <h1 className="text-center text-xl font-semibold">4 Year Plan</h1>
-            </div>
-            <CreditRange></CreditRange>
+      <div className="flex flex-col" data-testid="callYear">
 
-          {
-              //This displays all the year objects stored in the years array
-          }
           <div className="flex flex-col justify-center items-center min-h-screen">
-          
-              {
+
+                <h1 className="text-center text-xl font-semibold">4 Year Plan</h1>
+                {
+                    //Credit slider, change vals pushes changes to the min and max variables
+                }
+                <CreditRange changeVals={changeVals}></CreditRange>
+                {
+                  //Map years out to create the objects
                   years.map((year) => {     
                       return (
                           <Year name={year.name} className="flex-grow w-full" />
 
                       );
                   })
-              }
+                }
           </div>
             
             <button onClick={addYear} className="px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
--- src/components/CreditRange.js
+++ src/components/CreditRange.js
@@ -1,31 +1,55 @@
 import { useState } from "react";
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
+import Slider, { Range } from "rc-slider";
+import 'rc-slider/assets/index.css';
 function CreditRange(props) {
-    const [min, setMin] = useState();
-    const [max, setMax] = useState();
+    //Needed for the paragraph element displaying the range
+    const [low, setLow] = useState(12);
+    const [high, setHigh] = useState(20);
 
-    const changeVals = (newMin, newMax) => {
-        setMin(newMin);
-        setMax(newMax);
-        
+    //Function to pass changed values back to App.js
+    const updateVals = (value) => {
+        setLow(value[0])
+        setHigh(value[1])
+        props.changeVals(value)
     };
-
-    //const [minCredits, setMinCredits] = useState(null);
     return (
-        <div style={{ width: 300, margin: "50px auto" }}>
-            <Slider
-                range
-                defaultValue={ [12,19] }
-                pushable={ true}
-                count={1}
+        
+        <div style={{ width: "25%" }}>
+            {/* ^^ Controls the size of the slider and box for text*/}
+            <Slider range
                 min={0}
-                max={22}
-                
-                onChange={changeVals}
+                max={24}
+                defaultValue={[12, 20]}
+                count={1}
+                step={1}
+                onChangeComplete={updateVals}
+                pushable
+
+                trackStyle={{
+                    //stone 400
+                    backgroundColor: "#a1a1aa",
+                    height: 10
+                }}
+                railStyle={{
+                    // stone-100
+                    backgroundColor: "#f4f4f5",
+                    height: 10
+                }}
+                handleStyle={{
+                    // stone-500
+                    borderColor: "#71717a",
+                    backgroundColor: "white",
+                    height: 15,
+                    width: 15,
+                    marginLeft: 0,
+                    marginTop: -2.5
+                }}
             />
-            <p>Value: </p>
+
+            <p className="text-md font-semibold text-black text-center"> Preferred Semester Credit Range: {low} - { high}</p>
+
         </div>
+        
     );
 }
 
