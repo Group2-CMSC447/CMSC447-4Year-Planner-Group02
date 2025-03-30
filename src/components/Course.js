4660cd5be48953fd2d1b3d1520e2e537e9e701ca
rbarnes123 <rbarnes1@umbc.edu>
2025-03-30 09:56:48 -0400

    Got Modal to display Name and Description
    Details modal opens properly after API call. Correct course is found, data set. Still need to fix modal not displaying all information.

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,7 +1,16 @@
-import { useState} from 'react';
+import { useState, useEffect} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios'
 function Course(props) {
+    // declares local vars to hold course data for display.
+    const [courseData ,setCourseData] = useState({
+        description:"N/A",
+        credits: "N/A",
+        workload:"N/A",
+        attributes: "N/A",
+        preReqs: "N/A",
+    })
 
     //Drag function, collects needed data to pass to semester
     const onDragStart = (e) => {
@@ -11,10 +20,54 @@ function Course(props) {
         e.dataTransfer.setData("currYear", props.yearName);
         //Drop handling is done in the semester component only
     }
-    // Shows modal when details is clicked, hides when Close is clicked
+    // Shows modal when details button is clicked, hides when Close button is clicked
     const[show,setShow] = useState(false);
     const closeModal = () => setShow(false);
-    const openModal = () => setShow(true);
+
+
+    // opens modal and gets data for course if data exists for course
+    const openModal = () => {
+        let processing = true //used to fix some bug with react
+        axiosFetchCourses(processing) //function call to get actuall data from api
+        processing = false;
+    }
+
+    // holds course objects that api call returns
+    const [selectCourse, setSelectCourse] = useState([]);
+
+    const axiosFetchCourses = async(processing) =>{ //api call to get data [async = a function that can wait]
+        try{
+            const res = await axios.get('http://localhost:4000/courses') //request data from our backend running on port 4000
+            if (processing){
+                setSelectCourse(res.data) // set the select course array to whatever we got from api call
+            }
+            } 
+        catch(error) {console.error(error)}
+    }
+
+    // waits for api to populate course array
+    useEffect(() => {
+        // if course not empty, finds desired course, gets details
+        if(selectCourse.length > 0){
+            try{
+                const currCourse = selectCourse.find(item => item.name === props.name);
+                console.log(currCourse);
+                // sets variables to be printed within the modal
+                setCourseData(
+                    {description: currCourse.description},
+                    {credits: currCourse.credits},
+                    {workload: currCourse.workload},
+                    {attributes: currCourse.attributes},
+                    {preReqs: currCourse.preReqs}
+                )
+                // only shows modal if data exists
+                setShow(true);
+            }
+            // prints an error if course is not found in db
+            catch(error) {console.log("not found")}
+        }
+    },[selectCourse,props.name]);
+
     return (
         <div className="relative bg-white border border-gray-300 rounded-lg shadow-md space-y-2  flex-grow w-full max-h-sm"
             //Needed for drag and drop functionality
@@ -29,7 +82,7 @@ function Course(props) {
                  X
             </button>
             {/*course details button. Opens details modal when clicked*/}
-            <button className = "text-decoration: underline text-blue-600 text-right font-semibold hover:text-black"
+            <button className = "relative bottom-7 text-decoration: underline text-blue-600 text-right font-semibold hover:text-black"
                  onClick = {openModal}
             >
                 details 
@@ -46,9 +99,12 @@ function Course(props) {
                 </Modal.Header>
                 <Modal.Body>
 
-                    {/*description goes here*/}
-                    pull details from database for course based on props name
-
+                    {/*course details goes here*/}
+                    {courseData.description}
+                    {courseData.credits}
+                    {courseData.workload}
+                    {courseData.attributes}
+                    {courseData.preReqs}
                 </Modal.Body>
                 <Modal.Footer>
                 <Button variant="primary" onClick={closeModal}>Close</Button>
