89746de7c4357d166faa56d52aff89bca6742fef
rbarnes123 <rbarnes1@umbc.edu>
2025-04-02 10:16:58 -0400

    Course Details Works
    Got course details modal to open on click of details button. It calls the axios API, gets the course array, filters it for the one we clicked, gets the details of it, and then displays those details

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,4 +1,4 @@
-import { useState, useEffect} from 'react';
+import {useState,useEffect} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios'
@@ -9,7 +9,7 @@ function Course(props) {
         credits: "N/A",
         workload:"N/A",
         attributes: "N/A",
-        preReqs: "N/A",
+        preReqs: "N/A"
     })
 
     //Drag function, collects needed data to pass to semester
@@ -24,49 +24,54 @@ function Course(props) {
     const[show,setShow] = useState(false);
     const closeModal = () => setShow(false);
 
-
-    // opens modal and gets data for course if data exists for course
-    const openModal = () => {
-        let processing = true //used to fix some bug with react
-        axiosFetchCourses(processing) //function call to get actuall data from api
-        processing = false;
-    }
-
     // holds course objects that api call returns
     const [selectCourse, setSelectCourse] = useState([]);
 
-    const axiosFetchCourses = async(processing) =>{ //api call to get data [async = a function that can wait]
+    const axiosFetchCourses = async() =>{ //api call to get data [async = a function that can wait]
         try{
             const res = await axios.get('http://localhost:4000/courses') //request data from our backend running on port 4000
-            if (processing){
+            if (res){
                 setSelectCourse(res.data) // set the select course array to whatever we got from api call
             }
-            } 
+        } 
         catch(error) {console.error(error)}
     }
 
+    // calls function to get data for course which will
+    const openModal = () => {
+        axiosFetchCourses() //function call to get actuall data from api
+    }
+
     // waits for api to populate course array
-    useEffect(() => {
+    const findCourse = () => {
         // if course not empty, finds desired course, gets details
-        if(selectCourse.length > 0){
+        if(selectCourse){
             try{
-                const currCourse = selectCourse.find(item => item.name === props.name);
-                console.log(currCourse);
+                let currCourse = selectCourse.find(item => item.name === props.name);
+               
                 // sets variables to be printed within the modal
-                setCourseData(
-                    {description: currCourse.description},
-                    {credits: currCourse.credits},
-                    {workload: currCourse.workload},
-                    {attributes: currCourse.attributes},
-                    {preReqs: currCourse.preReqs}
-                )
+                setCourseData({
+                    description: currCourse.description,
+                    credits: currCourse.credits,
+                    workload: currCourse.workload,
+                    attributes: currCourse.attributes,
+                    preReqs: currCourse.preReqs
+                })
+            
                 // only shows modal if data exists
                 setShow(true);
             }
             // prints an error if course is not found in db
             catch(error) {console.log("not found")}
         }
-    },[selectCourse,props.name]);
+    }
+
+    // UseEffect to run findCourse when selectCourse updates
+    useEffect(() => {
+        if (selectCourse.length > 0) {
+            findCourse(); // Only runs when selectCourse has data
+        }
+    }, [selectCourse]); // Runs every time selectCourse updates
 
     return (
         <div className="relative bg-white border border-gray-300 rounded-lg shadow-md space-y-2  flex-grow w-full max-h-sm"
@@ -100,11 +105,13 @@ function Course(props) {
                 <Modal.Body>
 
                     {/*course details goes here*/}
-                    {courseData.description}
-                    {courseData.credits}
-                    {courseData.workload}
-                    {courseData.attributes}
-                    {courseData.preReqs}
+                    <div className = "flex flex-col gap-2 justify-center items-left ">
+                        {courseData.description}
+                        <p className= "flex">Credits: {courseData.credits}</p>
+                        <p className= "flex">Workload: {courseData.workload}</p>
+                        <p className= "flex">Attributes: {courseData.attributes}</p>
+                        <p className= "flex">Prerequisites: {courseData.preReqs}</p>
+                    </div>
                 </Modal.Body>
                 <Modal.Footer>
                 <Button variant="primary" onClick={closeModal}>Close</Button>
