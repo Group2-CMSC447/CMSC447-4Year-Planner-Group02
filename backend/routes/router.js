e9a0b2494ddeed7ae25a782e6cac1e079ba9666e
Caleb Sparks <cp.sparks1@gmail.com>
2025-04-03 19:47:44 -0400

    added CSV parsing for the CMSC Courses

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -2,6 +2,48 @@ const express = require('express')
 const router = express.Router()
 const schemas = require('../models/schemas')
 
+//get CSV from the google sheet
+const sheetID = "1_FpGoQveJlv1Egrse9kBKIOoEvJdk-6I3-dqA0mv20E";
+const sheetName = encodeURIComponent("CMSC Courses");
+const sheetURL = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:csv&sheet=${sheetName}`;
+
+fetch(sheetURL).then((response) => response.text()).then((csvText) => convertCSVCourseText(csvText));
+
+function convertCSVCourseText(csvText){
+    //console.log(csvText);
+    let courseObjects = csvToCourseObjects(csvText);
+    console.log(courseObjects);
+}
+
+function csvToCourseObjects(csvText){
+    const csvRows = csvText.split("\n");
+    const columnNames = splitCSVline(csvRows[0]);
+    const startOfData = 1;
+    let objects = [];
+
+    for(let i = startOfData; i < csvRows.length; ++i){
+        let curr = {};
+        let row = splitCSVline(csvRows[i]);
+        
+        for(let j = 0; j < row.length; ++j){
+            curr[columnNames[j]] = row[j];
+        }
+
+        objects.push(curr);
+    }
+    return objects;
+}
+
+function splitCSVline(line){
+    let splitLine = line.split('","');
+    splitLine[0] = splitLine[0].substring(1, );
+
+    splitLine[splitLine.length - 1] = splitLine[splitLine.length - 1].substring(0, splitLine[splitLine.length - 1].length - 1);
+    return splitLine;
+}
+
+
+
 // //will be needed later if we do webscraping to actually put the data into the DB instead of inputting it manually
 // router.post('/addCourse', async(req, res) => {
 //   const id = 'CMSC 104'
